fbe8c6e91085522f18df626d5dc697ab9aa7b619
FelixBieswanger <felix.bieswanger@googlemail.com>
2021-01-26 22:30:22 +0100

    pwa init

---
+++ pwa/script.js
@@ -0,0 +1,26 @@
+//PWA
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("serviceworker.js");
+}
+
+//Application Stuff
+var id = window.location.pathname;
+id = "id2";
+var source_simulation = new EventSource(
+  "http://localhost:2222/" + id + "/stream"
+);
+
+source_simulation.onopen = function (e) {
+  console.log("open");
+};
+source_simulation.addEventListener("message", function (e) {
+  let message;
+  try {
+    message = JSON.parse(e.data);
+  } catch {
+    message = e.data;
+  }
+
+  document.getElementById("data").innerHTML = message;
+});
