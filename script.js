611f9de269ffabfc9d6b56c937a9240d33b6923e
FelixBieswanger <felix.bieswanger@googlemail.com>
2021-01-28 22:32:21 +0100

    add notification

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("serviceworker.js");
 }
 
+//UI ELEMENTS
+var createAlertDialog = function () {
+  var dialog = document.getElementById("my-alert-dialog");
+
+  if (dialog) {
+    dialog.show();
+  } else {
+    ons
+      .createElement("alert-dialog.html", { append: true })
+      .then(function (dialog) {
+        dialog.show();
+      });
+  }
+};
+
+var hideAlertDialog = function () {
+  document.getElementById("my-alert-dialog").hide();
+};
+
+var notify = function () {
+  ons.notification.alert("This dialog was created with ons.notification");
+};
+
 /*
 
 //Application Stuff
