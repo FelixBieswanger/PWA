883403ecbb24da2e2671ae16b9f41e8399571e52
FelixBieswanger <felix.bieswanger@googlemail.com>
2021-02-02 01:03:10 +0100

    implement new chart

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
 //PWA
 
+var chart;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("serviceworker.js");
 }
 
-//UI ELEMENTS
-var createAlertDialog = function () {
-  var dialog = document.getElementById("my-alert-dialog");
-
-  if (dialog) {
-    dialog.show();
-  } else {
-    ons
-      .createElement("alert-dialog.html", { append: true })
-      .then(function (dialog) {
-        dialog.show();
-      });
-  }
-};
-
-var hideAlertDialog = function () {
-  document.getElementById("my-alert-dialog").hide();
-};
-
-var notify = function () {
-  ons.notification.alert("This dialog was created with ons.notification");
-};
-
 /*
+ ***************
+ * UI Elements *
+ ***************
+ */
+
+//Chart
+
+$(document).ready(function () {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: "doughnut",
+
+    // The data for our dataset
+    data: {
+      labels: ["blue"],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: ["rgb(54, 162, 235)"],
+          data: [10, 213],
+        },
+      ],
+    },
+
+    // Configuration options go here
+    options: {
+      cutoutPercentage: 80,
+      rotation: 1 * Math.PI,
+      circumference: 1 * Math.PI,
+    },
+  });
+});
 
 //Application Stuff
-var id = window.location.search.replace("?", "").replace("=", "");
+
+//var id = window.location.search.replace("?", "").replace("=", "");
+var id = 69;
 var source_simulation = new EventSource(
-  "https://192.168.178.142:2222/" + id + "/stream"
+  "https://lit-fortress-34268.herokuapp.com/id" + id + "/stream"
 );
 
 source_simulation.onopen = function (e) {
   console.log("open");
 };
 source_simulation.addEventListener("message", function (e) {
-  let message;
-  try {
-    message = JSON.parse(e.data);
-  } catch {
-    message = e.data;
-  }
-
-  document.getElementById("data").innerHTML = message;
+  let message = e.data;
+  chart.data.datasets[0].data[0] = message;
+  chart.update();
 });
-
-*/
